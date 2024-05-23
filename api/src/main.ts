0c106feb60c20f8e9e151aed4092b79b6eb778c4
Gurpal Singh <gillgurpal699@gmail.com>
2024-05-23 23:50:49 +0530

    [api] update packages and setup swc

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-
-import * as morgan from 'morgan';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
