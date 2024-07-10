491ebb9b71caa917a47a26ccc34540b99d632126
Gurpal Singh <gillgurpal699@gmail.com>
2024-07-10 21:41:28 +0530

    [api] add routes for streak shields

--- api/src/resources/streaks/streaks.controller.ts
+++ api/src/resources/streaks/streaks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { StreaksService } from './streaks.service';
 import { ApiHeader, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { Auth } from 'src/decorators/auth/auth.decorator';
@@ -33,5 +33,21 @@ export class StreaksController {
     @Param('month', ParseIntPipe) month: number,
   ) {
     return await this.streaksService.getUserStreaks(auth.id, month, year);
+  }
+
+  @ApiOperation({})
+  @Get('shields')
+  getStreakShields(@Auth() auth: User) {
+    return this.streaksService.getStreakShields(auth.id);
+  }
+
+  @Post('shields/one')
+  buyOneStreakShield(@Auth() auth: User) {
+    return this.streaksService.buyOneShield(auth.id);
+  }
+
+  @Post('shields/refill')
+  refillStreakShield(@Auth() auth: User) {
+    return this.streaksService.refillShields(auth.id);
   }
 }
--- api/src/resources/streaks/streaks.service.ts
+++ api/src/resources/streaks/streaks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { prisma } from 'src/db';
 
 @Injectable()
@@ -17,7 +17,7 @@ export class StreaksService {
       select: {
         createdAt: true,
         id: true,
-        type:true,
+        type: true,
       },
       orderBy: [
         {
@@ -26,5 +26,81 @@ export class StreaksService {
       ],
     });
     return streaks;
+  }
+
+  async getStreakShields(userId: string) {
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+    return {
+      shields: user.streakShields,
+    };
+  }
+
+  async buyOneShield(userId: string) {
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+    const shields = user.streakShields;
+    if (shields >= 5)
+      throw new HttpException(
+        'You cannot buy more streak shields',
+
+        HttpStatus.CONFLICT,
+      );
+
+    if (user.emeralds < 10) {
+      throw new HttpException(
+        'You do not have enough emeralds to refill streak shields',
+        HttpStatus.PAYMENT_REQUIRED,
+      );
+    }
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        streakShields: { increment: 1 },
+        emeralds: {
+          decrement: 10,
+        },
+      },
+    });
+    return {
+      id: userId,
+      emeralds: user.emeralds - 10,
+      shields: user.streakShields + 1,
+    };
+  }
+  async refillShields(userId: string) {
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+    const shields = user.streakShields;
+
+    if (shields >= 5) {
+      throw new HttpException(
+        'You cannot buy more streak shields',
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const shieldsToAdd = 5 - shields;
+    const totalCost = shieldsToAdd * 10;
+
+    if (user.emeralds < totalCost) {
+      throw new HttpException(
+        'You do not have enough emeralds to refill streak shields',
+        HttpStatus.PAYMENT_REQUIRED,
+      );
+    }
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        streakShields: shields + shieldsToAdd,
+        emeralds: {
+          decrement: totalCost,
+        },
+      },
+    });
+
+    return {
+      id: userId,
+      shields: 5,
+      emeralds: user.emeralds - totalCost,
+    };
   }
 }
